419dc9885499938b4031456faa14aa3552d8747a
Lorenzo Vaccarecci <98583823+GekkeijuITA@users.noreply.github.com>
2022-10-28 18:45:23 +0200

    Update largest_number_at_least_twice_of_others.js

--- largest_number_at_least_twice_of_others.js
+++ largest_number_at_least_twice_of_others.js
@@ -1,29 +1,34 @@
-const nums = [1,0];
-let l = nums.length;
-let max = 0;
-let maxN = nums[0];
-let isMax = false;
-for(let i = 0 ; i < l ; i ++)
-{
-    let n = nums[i];
-    let k = 1;
-    for(let j = 0 ; j < l ; j++)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+    let l = nums.length;
+    let max = 0;
+    let maxN = nums[0];
+    let isMax = false;
+    for(let i = 0 ; i < l ; i ++)
     {
-        if(n >= nums[j]*2)
+        let n = nums[i];
+        let k = 1;
+        for(let j = 0 ; j < l ; j++)
         {
-            k++;
+            if(n >= nums[j]*2)
+            {
+                k++;
+            }
+        }
+
+        if(k == l && n >= maxN)
+        {
+            max = i;
+            maxN = n;
+            isMax = true;
+        }
+        else if(!isMax)
+        {
+            max = -1;
         }
     }
-
-    if(k == l && n >= maxN)
-    {
-        max = i;
-        maxN = n;
-        isMax = true;
-    }
-    else if(!isMax)
-    {
-        max = -1;
-    }
-}
-console.log("Massimo: " + max);
+    return max;
+};
